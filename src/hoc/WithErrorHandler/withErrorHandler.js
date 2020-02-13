310f29efa264d169a44f92fcd11bf5b1c77f39f3
aswinks88 <aswinks88@gmail.com>
2020-02-14 00:22:19 +1300

    updated code

--- src/hoc/WithErrorHandler/withErrorHandler.js
+++ src/hoc/WithErrorHandler/withErrorHandler.js
@@ -8,13 +8,18 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
         componentWillMount () {
-            axios.interceptors.request.use(req => {
+            this.reqIntercep = axios.interceptors.request.use(req => {
                 this.setState({error: null})
                 return req
             })
-            axios.interceptors.response.use(res => res, error => {
+            this.resIntercep = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error})
             })
+        }
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.reqIntercep)
+            axios.interceptors.response.eject(this.resIntercep)
+
         }
         errorConfirmedHandler = () => {
 this.setState({error:null})
