562facd2c60a90253518b974721d56431289d5ce
aswinks88 <aswinks88@gmail.com>
2020-03-01 23:25:22 +1300

    Added routing and build a checkout container

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Burger from '../../Burger/Burger'
+import Button from '../../UI/Button/Button'
+import classes from './CheckoutSummary.css'
+const checkoutSummary = (props) => {
+return (
+    <div className={classes.CheckoutSummary}>
+        <h1>All is well</h1>
+        <div style={{width:'100%', margin:'auto'}}>
+            <Burger ingredients={props.ingredients}/>
+        </div>
+        <Button clicked btnType='Danger'>Cancel</Button>
+        <Button clicked btnType='Success'>Continue</Button>
+    </div>
+)
+}
+
+export default checkoutSummary
