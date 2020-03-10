bcf5353c5b00b68c63a823221c9ab5f19382edee
aswinks88 <aswinks88@gmail.com>
2020-03-11 00:03:36 +1300

    Added navigation item and orders page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import {Route, Switch} from 'react-router-dom'
+import Orders from './containers/Orders/Orders'
 class App extends Component {
 render(){
   return(
@@ -12,6 +13,7 @@ render(){
   <Checkout /> */}
   <Switch>
   <Route path='/checkout' component={Checkout}/>
+  <Route path='/orders' component={Orders} />
   <Route path='/' exact component={BurgerBuilder}/>
   </Switch>
 </Layout>
